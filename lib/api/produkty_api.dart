59b41acf12926927f6b62c7384a8716c30ab02d3
CDREApacher <83395940+CDREApacher@users.noreply.github.com>
2021-06-11 01:13:09 +0200

    Revert changes

--- lib/api/produkty_api.dart
+++ lib/api/produkty_api.dart
@@ -2,23 +2,14 @@ import 'package:dio/dio.dart';
 
 // Produkty
 
-/* BE
-
 Future<Response> fetchAll(Dio dio, int pgOffset){
   return dio.get("/data/Produkt", queryParameters: {'pageSize' : 100, 'offset' : pgOffset});
 }
-*/
-// *NEW*
-Future<Response> fetchAll(Dio dio/*, int pgOffset*/){
-  return dio.get("/products/all");
-}
-
-/* B
+/*
 Future<Response> fetchAll(Dio dio){
   return dio.get("/data/Produkt", queryParameters: {'pageSize' : 100});
 }
 */
-
 Future<Response> getCount(Dio dio){
   return dio.get("/data/Produkt", queryParameters: {'property' : 'Count(nazwaProduktu)'});
 }
@@ -26,30 +17,17 @@ Future<Response> getCount(Dio dio){
 Future<Response> fetch(Dio dio, String objectId){
   return dio.get("/data/Produkt/$objectId");
 }
-/* BE
+
 Future<Response> add(Dio dio, Map<String, dynamic> dane){
   return dio.post("/data/Produkt", data: dane);
 }
-*/
-
-// *NEW*
-Future<Response> add(Dio dio, Map<String, dynamic> dane){
-  return dio.post("/products", data: dane);
-}
-
 
 Future<Response> delete(Dio dio, String objectId){
   return dio.delete("/data/Produkt/$objectId");
 }
-/*
+
 Future<Response> update(Dio dio, String objectId, Map<String, dynamic> dane){
   return dio.put("/data/Produkt/$objectId", data: dane);
-}
-*/
-
-// *NEW*
-Future<Response> update(Dio dio, Map<String, dynamic> dane){
-  return dio.put("/products/update", data: dane);
 }
 
 // Return all products from given category
@@ -73,94 +51,39 @@ Future<Response> fetchKategorie(Dio dio){
   return dio.get("/data/Kategorie", queryParameters: {'pageSize' : 100});
 }
 
-
-
-
-
-
-// *NEW*
 Future<Response> fetchKategorieProdukty(Dio dio){
-  return dio.get("/category-product/all");
+  return dio.get("/data/Kategorie", queryParameters: {'pageSize' : 100, 'lista' : 'produkty'});
 }
 
-// *NEW*
 Future<Response> fetchKategorieZakupy(Dio dio){
-  return dio.get("/category-shopping/all");
+  return dio.get("/data/Kategorie", queryParameters: {'pageSize' : 100, 'lista' : 'zakupy'});
 }
 
-
-
-// *NEW*
-Future<Response> addKategorieProdukty(Dio dio, Map<String, dynamic> dane){
-  return dio.post("/category-product", data: dane);
-}
-
-// *NEW*
-Future<Response> addKategorieZakupy(Dio dio, Map<String, dynamic> dane){
-  return dio.post("/category-shopping", data: dane);
-}
-
-
-
-
-
-/* BE
 Future<Response> addKategorie(Dio dio, Map<String, dynamic> dane){
   return dio.post("/data/Kategorie", data: dane);
-}*/
+}
 
-/* BE
 Future<Response> deleteKategorie(Dio dio, String objectId){
   return dio.delete("/data/Kategorie/$objectId");
 }
-*/
-
 
 // Zakupy
-/*
+
 Future<Response> fetchZakupy(Dio dio){
   return dio.get("/data/Lista_Zakupow", queryParameters: {'pageSize' : 100});
 }
-*/
-Future<Response> fetchZakupy(Dio dio){
-  return dio.get("/shopping-list/all");
-}
-/*
+
 Future<Response> addZakup(Dio dio, Map<String, dynamic> dane){
   return dio.post("/data/Lista_Zakupow", data: dane);
 }
-*/
-
-// *NEW*
-Future<Response> addZakup(Dio dio, Map<String, dynamic> dane){
-  return dio.post("/shopping-list", data: dane);
-}
-
-
-Future<Response> deleteZakupy(Dio dio, String objectId){
-  return dio.delete("/shopping-list/$objectId");
-}
-
-
-
-Future<Response> buyProdukt(Dio dio, String produktId, int quantity) {
-  return dio.post("/shopping-list/buy/$produktId?quantity=$quantity");
-}
-
-
-
-
 
 Future<Response> updateZakup(Dio dio, String objectId, Map<String, dynamic> dane){
   return dio.put("/data/Lista_Zakupow/$objectId", data: dane);
 }
 
-/*
 Future<Response> deleteZakupy(Dio dio, String objectId){
   return dio.delete("/data/Lista_Zakupow/$objectId");
 }
-*/
-
 
 // Atrybuty
 
@@ -168,49 +91,21 @@ Future<Response> fetchAtrybutyById(Dio dio, String objectId){
   return dio.get("/data/Atrybuty?where=objectIdProdukt%3D'$objectId'", queryParameters: {'pageSize' : 100});
 }
 
-/* BE
 Future<Response> addAtrybut(Dio dio, Map<String, dynamic> dane){
   return dio.post("/data/Atrybuty", data: dane);
 }
- */
 
-
-// *NEW* ???????????????????????
-/*
-Future<Response> addAtrybut(Dio dio, String objectId, Map<String, dynamic> dane){
-  return dio.put("/products/$objectId", data: dane);
+Future<Response> deleteAtrybuty(Dio dio, String objectId){
+  return dio.delete("/data/Atrybuty/$objectId");
 }
-*/
-
-Future<Response> addAtrybut(Dio dio, String idProduktu, String nazwaAtrybutu){
-  return dio.put("/products/attribute/$idProduktu?attributeName=$nazwaAtrybutu");
-}
-
-Future<Response> deleteAtrybuty(Dio dio, String produktId, String atrybutId){
-  return dio.delete("/products/attribute/$produktId?attributeId=$atrybutId");
-}
-
 // Miary
 
-/* BE
 Future<Response> fetchMiary(Dio dio){
   return dio.get("/data/Miara", queryParameters: {'pageSize' : 100});
 }
-*/
 
-// *NEW*
-Future<Response> fetchMiary(Dio dio){
-  return dio.get("/measures/all");
-}
-
-/* BE
 Future<Response> addMiary(Dio dio, Map<String, dynamic> dane){
   return dio.post("/data/Miara", data: dane);
-}
-*/
-
-Future<Response> addMiary(Dio dio, Map<String, dynamic> dane){
-  return dio.post("/measures", data: dane);
 }
 
 Future<Response> deleteMiary(Dio dio, String objectId){
