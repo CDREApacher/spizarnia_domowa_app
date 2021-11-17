abb6c9ae53438213cdd2d163bfd34031b9695094
CDREApacher <marek.gulawski@wp.pl>
2021-11-17 14:17:15 +0100

    Quick change from local to remote hosting

--- lib/api/backendless_config.dart
+++ lib/api/backendless_config.dart
@@ -3,5 +3,5 @@
 //String queryUrl = "http://192.168.1.18:8080";
 //String restAPIKey = "4683DA39-31A6-4EAA-AFEA-45C2E17F23EC";
 /*-------------------------------------------*/
-String queryUrl = "http://10.0.2.2:8080";
-//String queryUrl = "http://46.41.141.26:8080";
+//String queryUrl = "http://10.0.2.2:8080";
+String queryUrl = "http://46.41.141.26:8080";
